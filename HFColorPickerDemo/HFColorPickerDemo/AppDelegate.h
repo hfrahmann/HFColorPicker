44c00145ef323e52988c26b177c8bd4dfd80eb5b
hendrik.frahmann <mail@hendrik-frahmann.de>
2014-04-30 16:33:07 +0200

    first version

---
+++ HFColorPickerDemo/HFColorPickerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HFColorPickerDemo
+//
+//  Created by Hendrik Frahmann on 30.04.14.
+//  Copyright (c) 2014 Hendrik Frahmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HFColorPickerDemo/HFColorPickerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HFColorPickerDemo
+//
+//  Created by Hendrik Frahmann on 30.04.14.
+//  Copyright (c) 2014 Hendrik Frahmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
